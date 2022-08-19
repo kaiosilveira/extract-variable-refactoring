576ee4bd51cbdafdb5220c4c5af0112261c9fa41
Kaio Silveira <silveira.kaio@icloud.com>
2022-08-19 20:02:36 +0100

    introduce quantityDiscount

--- function.js
+++ function.js
@@ -1,6 +1,7 @@
 function price(order) {
   // price is base price - quantity discount + shipping
   const basePrice = order.quantity * order.itemPrice;
+  const quantityDiscount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
   return (
     basePrice -
     Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
