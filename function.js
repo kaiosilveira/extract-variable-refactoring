446ff41b7edcf342e2ee9c923aee88fe95c05a33
Kaio Silveira <silveira.kaio@icloud.com>
2022-08-19 20:06:12 +0100

    remove now useless comment

--- function.js
+++ function.js
@@ -1,5 +1,4 @@
 function price(order) {
-  // price is base price - quantity discount + shipping
   const basePrice = order.quantity * order.itemPrice;
   const quantityDiscount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
   const shipping = Math.min(basePrice * 0.1, 100);
