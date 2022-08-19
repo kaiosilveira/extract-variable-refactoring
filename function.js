a196cf5af6886cef03f12063b6a9ba16a8d11de1
Kaio Silveira <silveira.kaio@icloud.com>
2022-08-19 20:04:16 +0100

    replace discount expression by its variable

--- function.js
+++ function.js
@@ -2,11 +2,7 @@ function price(order) {
   // price is base price - quantity discount + shipping
   const basePrice = order.quantity * order.itemPrice;
   const quantityDiscount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
-  return (
-    basePrice -
-    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
-    Math.min(basePrice * 0.1, 100)
-  );
+  return basePrice - quantityDiscount + Math.min(basePrice * 0.1, 100);
 }
 
 module.exports = { price };
