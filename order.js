02cf861106b4ad1a94b8e3d2722106aa8093b0ff
Kaio Silveira <silveira.kaio@icloud.com>
2022-08-20 15:37:33 +0100

    introduce shipping getter

--- order.js
+++ order.js
@@ -19,6 +19,10 @@ class Order {
     return Math.max(0, this.quantity - 500) * this.itemPrice * 0.05;
   }
 
+  get shipping() {
+    return Math.min(this.quantity * this.itemPrice * 0.1, 100);
+  }
+
   get price() {
     return (
       this.basePrice - this.quantityDiscount + Math.min(this.quantity * this.itemPrice * 0.1, 100)
