9844be3311a6044a477842daa03eafe6f0117876
Kaio Silveira <silveira.kaio@icloud.com>
2022-08-20 15:36:53 +0100

    use encapsulated quantityDiscount getter at Order.price

--- order.js
+++ order.js
@@ -21,9 +21,7 @@ class Order {
 
   get price() {
     return (
-      this.basePrice -
-      Math.max(0, this.quantity - 500) * this.itemPrice * 0.05 +
-      Math.min(this.quantity * this.itemPrice * 0.1, 100)
+      this.basePrice - this.quantityDiscount + Math.min(this.quantity * this.itemPrice * 0.1, 100)
     );
   }
 }
